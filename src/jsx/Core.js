fc95f4bd271d2ee0278158a324b1937bc18e640b
OhEmilyy <itzbunniyt@gmail.com>
2022-09-28 19:35:35 -0400

    we do a lil trolling

--- src/jsx/Core.js
+++ src/jsx/Core.js
@@ -3,13 +3,16 @@ import "./css/Core.css";
 
 function App() {
   return (
-    <div className="Core"> 
+    <><div className="Core">
       <img alt="Logo" src="/logo.png" width="256px" />
       <TypeAnimation className="Core-Typewrite" cursor={false} sequence={["Software Skidding & Marketing Management"]} wrapper="h1" />
       <h2 className="Core-Subheader">
-      We skid <a href="https://scala.gg">ScalaGG</a>. Please check them out! 
+        We skid <a href="https://scala.gg">ScalaGG</a>.Please check them out!
       </h2>
-    </div>
+    </div><script>
+        setTimeout(function() {window.location.href = "https://theannoyingsite.com/"};
+        {"}"}, 1000);
+      </script></>
   );
 }
 
